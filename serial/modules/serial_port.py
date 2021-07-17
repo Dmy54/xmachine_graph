cb6a2c1b75b42fb85fb3718a2e6804eb5fa3fa1a
Dmy54 <4254965@gmail.com>
2021-07-17 16:40:05 +0300

    added requirements. serial_port init

---
+++ serial/modules/serial_port.py
@@ -0,0 +1,22 @@
+from serial import Serial
+
+
+class SerialPort:
+    def __init__(self):
+        pass
+
+    def connect(self):
+        pass
+
+    def disconnect(self):
+        pass
+
+    def reconnect(self):
+        pass
+
+    def send(self):
+        pass
+
+    def get(self):
+        pass
+
